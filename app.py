babbb4e1146318fcac6bc064d8142513b9534f54
AniChikage <anichikage@163.com>
2021-06-17 15:02:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import random
 import json
 import requests
 
-option_a = os.getenv('OPTION_A', u"Cat 1111 🐺")
+option_a = os.getenv('OPTION_A', u"Cat 11112222 🐺")
 option_b = os.getenv('OPTION_B', u"Dog 🐶")
 hostname = socket.gethostname()
 
